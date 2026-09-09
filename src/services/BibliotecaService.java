55a1d73f90efd6d9381a600fe5b7deeaeb693f96
ViniciusProgrammer <vinicios.david.083@ufrn.edu.br>
2026-09-09 20:11:41 -0300

    refactor: mudada a lógica de pegar emprestado o livro

--- src/services/BibliotecaService.java
+++ src/services/BibliotecaService.java
@@ -5,6 +5,7 @@ import entities.Livro;
 import entities.Usuario;
 import exceptions.DadosInvalidosExceptions;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +15,8 @@ public class BibliotecaService {
     private final List<EmprestimoLivro> livrosEmprestados = new ArrayList<>();
 
     public void cadastrarLivro(String titulo, String autor, String isbn, int quantidade) throws DadosInvalidosExceptions {
-            Livro livro = new Livro(titulo, autor, isbn, quantidade);
-            this.livros.add(livro);
+        Livro livro = new Livro(titulo, autor, isbn, quantidade);
+        this.livros.add(livro);
     }
 
     public void cadastrarUsuario(String nome, String email, String telefone) {
@@ -27,58 +28,37 @@ public class BibliotecaService {
         cadastrarUsuario(usuario.getNome(), usuario.getEmail(), usuario.getTelefone());
     }
 
-    /*
-    public EmprestimoLivro emprestarLivro(String isbn, String emailUsuario) {
-        // FALTA A IMPLEMENTAÇÃO DA LÓGICA COMPLEXA AQUI
-    }
-    */
-
-//    public void pegarLivroEmprestado(String nomeDoLivro, int quantidadeCopiasSolicitadas) {
-//        try {
-//            if (!livro.getTitulo().equals(nomeDoLivro) || livro.getQuantidadeDisponivel() <= 0) {
-//                throw new LivroIndisponivelException("O livro solicitado está sem disponibilidade no momento");
-//            }
-//
-//            for (Livro livro : livros) {
-//                if (livro.getTitulo().equals(nomeDoLivro)) {
-//                    // Precisa elaborar uma lógica para decrementar isso aqui
-//                    //   livro.getQuantidadeDisponivel() -= quantidadeCopiasSolicitadas;
-//                }
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-
-    public boolean devolverLivro(String isbn, String emailUsuario, int quantidade) {
-        boolean isbnEValido = false;
-        boolean emailEValido = false;
-        boolean quantidadeEValida = false;
+    public EmprestimoLivro emprestarLivro(String tituloLivro, String emailUsuario, int quantidadePegarEmprestado) {
+        boolean temLivroDisponivel = false;
+        boolean usuarioExiste = false;
+        Livro livroRetornado = null;
+        Usuario usuarioRetornado = null;
 
         for (Livro livro : livros) {
-            if (livro.getIsbn().equals(isbn)) {
-                // Consertar o erro aqui
-                //  livro.devolverLivro(quantidade);
-                isbnEValido = true;
+            if (livro.getTitulo().equals(tituloLivro)) {
+                if (livro.getQuantidadeDisponivel() > 0 && livro.getQuantidadeDisponivel() <= quantidadePegarEmprestado) {
+                    livro.removerExemplar(quantidadePegarEmprestado);
+                    livroRetornado = livro;
+                    temLivroDisponivel = true;
+                }
             }
         }
 
         for (Usuario usuario : usuarios) {
             if (usuario.getEmail().equals(emailUsuario)) {
-                emailEValido = true;
+                usuarioRetornado = usuario;
+                usuarioExiste = true;
             }
         }
 
-        if (quantidade > 0) {
-            quantidadeEValida = true;
-        }
-
-        if (isbnEValido && emailEValido && quantidadeEValida) {
-            return true;
-        }
-
-        return false;
+        return new EmprestimoLivro(livroRetornado, usuarioRetornado, emailUsuario);
     }
+
+    /*
+    public EmprestimoLivro devolverLivro(String isbn, String emailUsuario, int quantidadeDevolvida, LocalDate dataDevolucao) {
+
+    }
+    */
 
     public void listarLivrosDisponiveis() {
         for (Livro livro : livros) {
