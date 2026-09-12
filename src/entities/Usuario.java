1c1594f9bb6aee5afcf4a0cbcdca51a4f8d13ef1
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-09-12 09:12:36 -0300

    refactor: Mudelegada a validação dentro do contrutor e a forma como o usuario pega emprestado livro

--- src/entities/Usuario.java
+++ src/entities/Usuario.java
@@ -1,29 +1,34 @@
 package entities;
 
 import exceptions.DadosInvalidosExceptions;
+import exceptions.UsuarioBloqueadoException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario {
-    private String nome;
-    private String email;
-    private String telefone;
+    private final String nome;
+    private final String email;
+    private final String telefone;
     private double multa;
-    private List<EmprestimoLivro> livrosPegosImprestadosAtivo = new ArrayList<>(3);
+    private final List<EmprestimoLivro> livrosPegosImprestadosAtivo = new ArrayList<>(3);
 
-    public Usuario(String nome, String email, String telefone) {
-        if (validarNome(nome)) {
-            this.nome = nome;
+    public Usuario(String nome, String email, String telefone) throws DadosInvalidosExceptions {
+        if (nome == null || nome.isBlank()) {
+            throw new DadosInvalidosExceptions("O nome não pode ser nulo ou vazio");
         }
 
-        if (validarEmail(email)) {
-            this.email = email;
+        if (email == null || email.isBlank()) {
+            throw new DadosInvalidosExceptions("O email não pode ser nulo ou vazio");
         }
 
-        if (validarTelefone(telefone)) {
-            this.telefone = telefone;
+        if (telefone == null || telefone.isBlank()) {
+            throw new DadosInvalidosExceptions("O telefone não pode ser nulo ou vazio");
         }
+
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
     }
 
     public String getNome() {
@@ -38,16 +43,17 @@ public class Usuario {
         return telefone;
     }
 
-//    public boolean podePegarLivro(String isbn, int quantidadeSolicitada) {
-//        try {
-//            if (this.livrosPegosImprestadosAtivo.size() < 3 && multa <= 10.0) {
-//
-//            }
-//        } catch (Exception e) {
-//
-//        }
-//        //retorna true se tiver menos de 3 empréstimos ativos e multa < 10
-//    }
+    public void podePegarLivro(Livro livro, Usuario usuario) {
+        try {
+            this.livrosPegosImprestadosAtivo.add(new EmprestimoLivro(livro, usuario));
+
+            if (this.livrosPegosImprestadosAtivo.size() > 3 && multa > 10.0) {
+                throw new UsuarioBloqueadoException("O usuário não pode pegar livro no momento");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     public void adicionarMulta() {
 
@@ -55,45 +61,6 @@ public class Usuario {
 
     public void pagarMulta() {
 
-    }
-
-    private boolean validarNome(String nome) {
-        try {
-            if (nome == null || nome.isBlank()) {
-                throw new DadosInvalidosExceptions("Nome do usuário não pode ser nulo ou vázio");
-            }
-
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-
-    private boolean validarEmail(String email) {
-        try {
-            if (email == null || email.isBlank() || !email.contains("@")) {
-                throw new DadosInvalidosExceptions("O email precisa ser válido");
-            }
-
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-
-    private boolean validarTelefone(String telefone) {
-        try {
-            if (telefone == null || telefone.isBlank()) {
-                throw new DadosInvalidosExceptions("O telefone não é válido");
-            }
-
-            return true;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return false;
-        }
     }
 
     @Override
